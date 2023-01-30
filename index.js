bd4e0ddbb55be10ecdfd82c445f87282a2fe0c67
ojpbarbosa <joaopedrofbarbosa@protonmail.com>
2023-01-29 23:01:19 -0300

    chore: give up node and express

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-
-const app = express();
-const port = process.env.PORT || 3030;
-
-app.get("/", (request, response) => response.send("twilightime"));
-
-app.listen(port);
